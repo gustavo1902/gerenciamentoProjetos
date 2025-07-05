f0c5752cab160633fc21bf7911e1d21948e41bad
gustavo1902 <gustavomorais1009@gmail.com>
2025-07-05 15:14:42 -0300

    feat: cors implementação

---
+++ taskpilot-backend/src/main/java/com/taskpilot/taskpilot_backend/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.taskpilot.taskpilot_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Permite CORS para todos os endpoints sob /api
+                .allowedOrigins("http://localhost:8081", "http://127.0.0.1:8081", "http://seu-dominio-frontend.com") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
