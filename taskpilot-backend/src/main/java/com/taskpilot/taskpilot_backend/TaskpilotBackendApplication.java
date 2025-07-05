e8525cf22213614920c6eaa8ce81e323b06422ef
gustavo1902 <gustavomorais1009@gmail.com>
2025-07-05 02:22:05 -0300

    feat: spring initializer

---
+++ taskpilot-backend/src/main/java/com/taskpilot/taskpilot_backend/TaskpilotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.taskpilot.taskpilot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskpilotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskpilotBackendApplication.class, args);
+	}
+
+}
---
+++ taskpilot-backend/src/test/java/com/taskpilot/taskpilot_backend/TaskpilotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskpilot.taskpilot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskpilotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
